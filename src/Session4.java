64258d26780eded15bbf2db3d5abcad4a785fbe2
Phani Madiraju <phanimkms1@gmail.com>
2023-08-07 13:01:19 +0530

    Session4 code committed

---
+++ src/Session4.java
@@ -0,0 +1,89 @@
+import java.util.*;
+
+public class Session4 {
+    public static void main(String[] args) {
+        //Arrays
+//        int number=10;
+//        char character='a';
+//        int[] numbers=new int[5];
+//        System.out.println(numbers[1]);
+        //numbers[0],numbers[1],numbers[2],numbers[3],numbers[4];
+//        System.out.println(numbers[5]);
+
+//        System.out.println("hello");
+//        System.out.println("hello");
+//        System.out.println("hello");
+//        System.out.println("hello");
+//        System.out.println("hello");
+//        for(int i=0;i<5;i++){
+//            System.out.println("hello");
+//        }
+       /* Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the Array  ");
+        int size=sc.nextInt();
+        int[] numbers=new int[size];
+        for(int i=0;i<size;i++){
+            System.out.println("Please enter the number at index :"+i);
+         numbers[i]=sc.nextInt();
+        }
+        for(int i=0;i<size;i++){
+            System.out.println("The element at index :"+i+" is :"+numbers[i]);
+        }*/
+        //== comparision operator , = assignment operator
+      //  System.out.println(5!=5);
+       /* int no1=5;
+        int no2=10;
+        if(no1==no2) {
+            System.out.println("both numbers are equal");
+        }else{
+            System.out.println("both numbers are different ");
+        }*/
+
+        //conditional statements
+        //if , if else , else if , nested if else
+       /* Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your age ");
+        int age= sc.nextInt();
+        if(age>=21){
+            System.out.println("Major , You can enter the PUB ");
+        }else{
+            System.out.println("Minor, You are not allowed to enter  ");
+        }*/
+        /*Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your score ");
+        int score= sc.nextInt();
+        if(score>=70){
+            System.out.println("Grade A");
+        }else if(score >=60 && score<70){
+            System.out.println("Grade B");
+        }else if (score>=50 && score <60){
+            System.out.println("Grade C");
+        }else{
+            System.out.println("Grade D");
+        }*/
+
+        /*//Program 3
+        //"I want to learn Java"
+        //Learn
+        String sentence="I want to learn java";
+        String extractedWord=sentence.substring(10,15);
+        //learn
+       // System.out.println(extractedWord);
+        //charAt
+        char extractedChar=extractedWord.charAt(0);
+        //System.out.println(extractedChar);
+        String word2=extractedWord.substring(1,5);
+        //System.out.println(word2);
+        char capital_L=Character.toUpperCase(extractedChar);
+        //System.out.println(capital_L);
+        String answer=capital_L+word2;
+        System.out.println("Expected output : "+answer);*/
+
+
+//start with nested if else tomorrow
+        //for loop
+        //while
+        //do while
+
+    }
+}
